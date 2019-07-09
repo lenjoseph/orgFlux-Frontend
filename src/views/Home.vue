1ed15d62c67a6e4817ebb3aac7dd1a9cbc0e5184
Len Joseph <ldjoseph005@gmail.com>
2019-07-09 10:01:23 -0400

    Update Home.vue

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -38,6 +38,15 @@ $darkColor: #1e737c;
 $lightColor: #75e1dd;
 $secondaryColor: #f7e291;
 
+.component-fade-enter-active,
+.component-fade-leave-active {
+  transition: opacity 0.15s ease;
+}
+.component-fade-enter,
+.component-fade-leave-to {
+  opacity: 0;
+}
+
 .home {
   height: calc(100% - 116px);
   border-top: 1px solid rgba(grey, 0.2);
